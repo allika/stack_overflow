545b130befb443b2fa69376879e30cdf6bf27bdd
Alla <a.lisitska@gmail.com>
2012-06-13 18:52:06 +0300

    attributes names has been modified accordingly to the migration structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :username, :email, :password, :created_at, :updated_at
+  attr_accessible :first_name, :last_name, :username, :email, :hashed_password, :salt, :created_at, :updated_at
 end

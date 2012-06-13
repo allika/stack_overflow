a91c02fbdb7aed372c1d9ed69e7ffddbad969385
Alla Lisitskaya <a.lisitska@gmail.com>
2012-06-13 10:31:49 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackOverflow::Application.config.secret_token = '51b2b25d27ab55794e5021c977a34a272304c486fcf9e4a8571b205e699f624ef5852654743cba22d0975552a8ad922c9a3a930c6fba3937cad1cbae60bc1ff6'

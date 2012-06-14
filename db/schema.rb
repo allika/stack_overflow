b9f1048694b72416dc49459624684ba67b52e993
Alla <a.lisitska@gmail.com>
2012-06-14 19:17:43 +0300

    db changes related

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120613152213) do
+ActiveRecord::Schema.define(:version => 20120614112542) do
 
   create_table "categories", :force => true do |t|
     t.string   "name",       :limit => 200

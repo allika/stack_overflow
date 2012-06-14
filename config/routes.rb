7b3b172e27b9c7a900d7ac5db028aa479fa25371
Alla <a.lisitska@gmail.com>
2012-06-14 19:14:03 +0300

    resources has been added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 StackOverflow::Application.routes.draw do
+
+  resources :categories
+  resources :themes
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

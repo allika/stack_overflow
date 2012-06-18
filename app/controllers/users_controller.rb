b2f53d0963b1e1195216860a370acf9ed4be8859
Alla <a.lisitska@gmail.com>
2012-06-18 15:59:04 +0300

    small change for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:notice] = "User has been created"
+      flash[:notice] = "Your account has been created"
       redirect_to @user
     else
       render 'new'

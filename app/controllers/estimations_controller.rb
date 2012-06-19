77b38d6ed64c78efe2ef2287a169f43d73576362
Alla <a.lisitska@gmail.com>
2012-06-19 15:43:39 +0300

    for registered users has been added ability to give ones +1/-1 for any comment's rating

--- app/controllers/estimations_controller.rb
+++ app/controllers/estimations_controller.rb
@@ -1,2 +1,22 @@
 class EstimationsController < ApplicationController
+
+  before_filter :find_comment
+
+  def create
+    @estimation = Estimation.new(params[:estimation])
+    if @estimation.save
+      redirect_to comments_path(:theme_id => @comment.theme_id)
+    else
+      flash[:error] = "Something goes wrong!"
+      redirect_to comments_path(:theme_id => @comment.theme_id)
+    end
+  end
+
+  private
+
+  def find_comment
+    if params[:estimation][:comment_id]
+      @comment = Comment.find_by_id(params[:estimation][:comment_id])
+    end
+  end
 end

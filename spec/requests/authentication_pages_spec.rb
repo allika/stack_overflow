c2624fd5e6e24bb9a6d9992969443a60540b6612
Alla <a.lisitska@gmail.com>
2012-06-19 21:07:10 +0300

    spec related files

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1',    text: 'Sign in') }
+  end
+end

b4d89d76271ce35616eb9732399a377c3dca17a0
Alla <a.lisitska@gmail.com>
2012-06-22 17:10:08 +0300

    has been correct redirection path after tag_attaching creation, and added 'destroy' method

--- app/controllers/tag_attachings_controller.rb
+++ app/controllers/tag_attachings_controller.rb
@@ -1,7 +1,7 @@
 class TagAttachingsController < ApplicationController
 
   before_filter :find_tag
-  before_filter :find_comment
+  before_filter :find_comment_theme
 
   def create
     @tag_attaching = TagAttaching.new(params[:tag_attaching])
@@ -9,11 +9,17 @@ class TagAttachingsController < ApplicationController
       @current_tag_popularity = @tag.popularity + 1
       @tag.update_attributes!(:popularity => @current_tag_popularity)
       flash[:notice] = "Tag #{@tag.name} has been attached."
-      redirect_to comments_path( :theme_id => @comment.theme_id )
+      redirect_to category_themes_path( :category_id => @theme.category_id )
     else
-      flash[:error] = "Something goes wrong. Tag wasn't attached'"
-      redirect_to comments_path( :theme_id => @comment.theme_id )
+      flash[:notice] = "Something goes wrong...Tag wasn't attached"
+      redirect_to category_themes_path( :category_id => @theme.category_id )
     end
+  end
+
+  def destroy
+    TagAttaching.find(params[:id]).destroy
+    flash[:notice] = "Tag has been detached"
+     redirect_to comments_path( :theme_id => @comment.theme_id )
   end
 
   private
@@ -24,9 +30,10 @@ class TagAttachingsController < ApplicationController
     end
   end
 
-  def find_comment
+  def find_comment_theme
     if params[:tag_attaching][:comment_id]
       @comment = Comment.find_by_id(params[:tag_attaching][:comment_id])
+      @theme = Theme.find_by_id(@comment.theme_id)
     end
   end
 end

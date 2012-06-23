4b07fd79c7d05a8fc247ebaedb6650fb94042c3b
Alla <a.lisitska@gmail.com>
2012-06-23 11:48:30 +0300

    tag_attachings_controller has been cleaned

--- app/controllers/tag_attachings_controller.rb
+++ app/controllers/tag_attachings_controller.rb
@@ -18,8 +18,6 @@ class TagAttachingsController < ApplicationController
 
   def destroy
     @tag_attaching = TagAttaching.find(params[:id]).destroy
-    @comment = Comment.find_by_id(@tag_attaching.comment_id)
-    @theme = Theme.find_by_id(@comment.theme_id)
     flash[:notice] = "Tag has been detached"
     redirect_to category_themes_path( :category_id => @theme.category_id )
   end
@@ -33,9 +31,12 @@ class TagAttachingsController < ApplicationController
   end
 
   def find_comment_theme
-    if params[:tag_attaching] && params[:tag_attaching][:comment_id]
-      @comment = Comment.find_by_id(params[:tag_attaching][:comment_id])
+    comment_id = if ( params[:tag_attaching] && params[:tag_attaching][:comment_id] ) then
+                      params[:tag_attaching][:comment_id]
+                 elsif params[:id] then
+                      TagAttaching.find_by_id(params[:id]).comment_id
+                 end
+      @comment = Comment.find_by_id(comment_id)
       @theme = Theme.find_by_id(@comment.theme_id)
-    end
   end
 end

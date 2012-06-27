6845344cab5d3586bf53c3ea44b92b3c9a4f406e
Alla <a.lisitska@gmail.com>
2012-06-27 14:46:27 +0300

    has been added edit and update actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Changes have been save"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
